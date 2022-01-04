42bd69ec812bc7a079de887a2d42489dd7e2f2b8
diaryhub <kjw991953@gmail.com>
2022-01-04 14:46:04 +0900

    test

---
+++ src/com/study/s1/method/ReturnStudy.java
@@ -0,0 +1,11 @@
+package com.study.s1.method;
+
+public class ReturnStudy {
+	
+	//접근지정자 [그외지정자] 리턴타입 메서드명([매개변수 선언]){}
+	public void test1(){
+		
+		System.out.println("void는 리턴이 없을 때 사용");
+	
+	}
+}
