cd2077352e7e118ea4da1453cafb13bcc910a827
diaryhub <kjw991953@gmail.com>
2022-01-04 16:37:06 +0900

    study

--- src/com/study/s1/method/MethodMain.java
+++ src/com/study/s1/method/MethodMain.java
@@ -1,12 +1,31 @@
 package com.study.s1.method;
 
+import java.util.Scanner;
+
 public class MethodMain {
 
 	public static void main(String[] args) {
-
-		System.out.println("MethodMain Start");
-		System.out.println("MethodMain Finish");
 		
+		
+		System.out.println("MethodMain Start");
+		
+		
+		ReturnStudy rs = new ReturnStudy();
+		
+		rs.test1();
+		
+		rs.test2();
+		
+		System.out.println(rs.test2());
+		
+
+		System.out.println(rs.test3());
+		
+		Scanner sc = rs.test4();
+		
+		
+		
+		System.out.println("MethodMain Finish");
 		
 	}
 
--- src/com/study/s1/method/ReturnStudy.java
+++ src/com/study/s1/method/ReturnStudy.java
@@ -1,11 +1,39 @@
 package com.study.s1.method;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class ReturnStudy {
-	
-	//접근지정자 [그외지정자] 리턴타입 메서드명([매개변수 선언]){}
-	public void test1(){
-		
+
+	// 접근지정자 [그외지정자] 리턴타입 메서드명([매개변수 선언]){}
+	public void test1() {
+
 		System.out.println("void는 리턴이 없을 때 사용");
+
+	}
+
+	public int test2() {
+
+		Random rd = new Random();
+
+		int num = rd.nextInt();
+
+		return num;
+	}
+
+	public String test3() {
+		Scanner sc = new Scanner(System.in);
+		// 키보드로부터 이름 입력 받고 리턴
+		System.out.print("이름 입력:");
+		
+		String name = sc.next();
+
+		return name;
+	}
 	
+	public Scanner test4() {
+		Scanner sc = new Scanner(System.in);
+		
+		return sc;
 	}
 }
